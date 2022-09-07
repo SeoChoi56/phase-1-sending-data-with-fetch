822133fcb7a89648a3ea6324bec8e3ecae2ff28a
Seo Choi <seochoi56@gmail.com>
2022-09-06 20:47:33 -0400

    commit tree a4e6ebb07117e38200844d8b3d27a4ee15a9895c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(myName, myEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: myName,
+            email: myEmail
+        })
+    })
+    .then(res => res.json())
+    .then(function(data) {document.querySelector("body").append(data.id)})
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        console.log(error.message);
+        document.querySelector("body").append(error.message)
+    })
+    
+}
+
+submitData("seo", "42WallabyWAY@gmail.com");
